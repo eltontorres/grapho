574b18f9b3439e26f77e4bb28fe03e3f1ee38fb6
Elton Torres <e.s.torres@outlook.com>
2024-08-28 23:35:01 -0300

    modificação e adição da função para o produto cartesiano

--- src/graph.rs
+++ src/graph.rs
@@ -1,31 +1,32 @@
 use std::collections::{HashMap, HashSet};
 
 #[derive(Debug)]
-pub struct Graph {
-    adjacency_matrix: Vec<Vec<i64>>,
-    adjacency_list: HashMap<i64, HashSet<i64>>,
-    vertices: HashSet<i64>,
-    edges: HashSet<(i64, i64)>,
-    directed: bool,
+pub struct GraphMatrix {
+    adjacency_matrix: Vec<Vec<usize>>,
+    vertices: HashSet<usize>,
+    edges: HashSet<(usize, usize)>,
+}
+#[derive(Debug)]
+pub struct GraphList {
+    adjacency_list: HashMap<usize, HashSet<usize>>,
+    vertices: HashSet<usize>,
+    edges: HashSet<(usize, usize)>,
 }
 
-impl Graph {
+impl GraphMatrix {
     pub fn new() -> Self {
-        Graph {
+        GraphMatrix {
             adjacency_matrix: Vec::new(),
-            adjacency_list: HashMap::new(),
             vertices: HashSet::new(),
             edges: HashSet::new(),
-            directed: true,
         }
     }
 
-    fn add_vertex(&mut self, vertex: i64) {
+    fn add_vertex(&mut self, vertex: usize) {
         self.vertices.insert(vertex);
-        self.adjacency_list.insert(vertex, HashSet::new());
     }
 
-    fn set_edge(&mut self, vertex1: i64, vertex2: i64) {
+    fn set_edge(&mut self, vertex1: usize, vertex2: usize) {
         if self.vertices.contains(&vertex1) == false {
             self.add_vertex(vertex1);
         }
@@ -35,7 +36,7 @@ impl Graph {
         }
     }
 
-    pub fn add_edges(mut self, edges: &Vec<(i64, i64)>) -> Self {
+    pub fn add_edges(mut self, edges: &Vec<(usize, usize)>) -> Self {
         for edge in edges {
             self.set_edge(edge.0, edge.1);
             self.edges.insert(*edge);
@@ -44,27 +45,111 @@ impl Graph {
         self
     }
 
-    pub fn build_adjacency_list(mut self) -> Self {
-        for edge in self.edges.iter() {
-            self.adjacency_list.get_mut(&edge.0).unwrap().insert(edge.1);
-            if self.directed == false {
-                self.adjacency_list.get_mut(&edge.1).unwrap().insert(edge.0);
-            }
-        }
-        self
-    }
-
-    pub fn build_adjacency_matrix(mut self) -> Self {
+    pub fn build_oriented(mut self) -> Self {
         let max_vertex = *self.vertices.iter().max().unwrap() as usize + 1;
         self.adjacency_matrix = vec![vec![0; max_vertex]; max_vertex];
 
         for edge in self.edges.iter() {
-            self.adjacency_matrix[edge.0 as usize][edge.1 as usize] = 1;
-            if self.directed == false {
-                self.adjacency_matrix[edge.1 as usize][edge.0 as usize] = 1;
-            }
+            self.adjacency_matrix[edge.0][edge.1] = 1;
         }
 
         self
     }
+
+    pub fn build_no_oriented(mut self) -> Self {
+        let max_vertex = *self.vertices.iter().max().unwrap() as usize + 1;
+        self.adjacency_matrix = vec![vec![0; max_vertex]; max_vertex];
+
+        for edge in self.edges.iter() {
+            self.adjacency_matrix[edge.0][edge.1] = 1;
+            self.adjacency_matrix[edge.1][edge.0] = 1;
+        }
+
+        self
+    }
+
+    pub fn contains_edge(&self, vertex: &(usize, usize)) -> bool {
+        let (vertex1, vertex2) = vertex;
+        self.adjacency_matrix[*vertex1][*vertex2] > 0
+    }
+}
+
+impl GraphList {
+    pub fn new() -> Self {
+        GraphList {
+            adjacency_list: HashMap::new(),
+            vertices: HashSet::new(),
+            edges: HashSet::new(),
+        }
+    }
+
+    fn add_vertex(&mut self, vertex: usize) {
+        self.vertices.insert(vertex);
+        self.adjacency_list.insert(vertex, HashSet::new());
+    }
+
+    fn set_edge(&mut self, vertex1: usize, vertex2: usize) {
+        if self.vertices.contains(&vertex1) == false {
+            self.add_vertex(vertex1);
+        }
+
+        if self.vertices.contains(&vertex2) == false {
+            self.add_vertex(vertex2);
+        }
+    }
+
+    pub fn add_edges(mut self, edges: &Vec<(usize, usize)>) -> Self {
+        for edge in edges {
+            self.set_edge(edge.0, edge.1);
+            self.edges.insert(*edge);
+        }
+
+        self
+    }
+
+    pub fn build_oriented(mut self) -> Self {
+        for edge in self.edges.iter() {
+            self.adjacency_list.get_mut(&edge.0).unwrap().insert(edge.1);
+        }
+        self
+    }
+
+    pub fn build_no_oriented(mut self) -> Self {
+        for edge in self.edges.iter() {
+            self.adjacency_list.get_mut(&edge.0).unwrap().insert(edge.1);
+            self.adjacency_list.get_mut(&edge.1).unwrap().insert(edge.0);
+        }
+        self
+    }
+
+    pub fn contains_edge(&self, vertex: &(usize, usize)) -> bool {
+        let (vertex1, vertex2) = vertex;
+        match self.adjacency_list.get(&vertex1) {
+            Some(edges) => edges.contains(&vertex2),
+            None => false,
+        }
+    }
+}
+
+pub fn cartesian_graph_list(g1: GraphList, g2: GraphList) -> GraphList {
+    let mut new_vertices: Vec<(usize, usize)> = Vec::new();
+
+    for v in g1.vertices.iter() {
+        for u in g2.vertices.iter() {
+            new_vertices.push((*v, *u));
+        }
+    }
+    let mut new_edges: Vec<(usize, usize)> = Vec::new();
+    for (index_1, elem_1) in new_vertices.iter().enumerate() {
+        for (index_2, elem_2) in new_vertices.iter().enumerate() {
+            if elem_1.0 == elem_2.0 && g2.contains_edge(&(elem_1.1, elem_2.1)) {
+                new_edges.push((index_1, index_2));
+            }
+            if elem_1.1 == elem_2.1 && g1.contains_edge(&(elem_1.0, elem_2.0)) {
+                new_edges.push((index_1, index_2));
+            }
+        }
+    }
+
+    GraphList::new().add_edges(&new_edges).build_no_oriented()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 mod graph;
-use graph::Graph;
+use graph::{GraphList, cartesian_graph_list};
 
 fn main() {
-    let graph = Graph::new().add_edges(&vec![(1, 2), (2, 3), (3, 4), (4, 1)]).build_adjacency_matrix();
+    let graph1 = GraphList::new()
+        .add_edges(&vec![(1, 2), (2, 3), (3, 4), (4, 1)])
+        .build_no_oriented();
+    let graph2 = GraphList::new()
+        .add_edges(&vec![(1, 4), (2, 6), (3, 2), (3, 1)])
+        .build_no_oriented();
 
-    println!("{:?}", graph);
+    print!("Graph 1: {:?}\n", graph1);
+    print!("Graph 2: {:?}\n", graph2);
+
+    let result = cartesian_graph_list(graph1, graph2);
+    print!("Result graph: {:?}\n", result);
 }
